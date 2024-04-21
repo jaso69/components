c421143bf531b9ce1347d85dc2d1474fde162a18
jaso69 <joseonieva@gmail.com>
2024-04-21 13:05:28 +0200

    add status

--- main.js
+++ main.js
@@ -33,6 +33,40 @@ clases_el.forEach((el) => {
 
 /*** menu***************************************/
 
+let status = 'default';
+const defaultEl = document.querySelector('#defaultEl')
+const activeEl = document.querySelector('#activeEl')
+const focusEl = document.querySelector('#focusEl')
+const hoverEl = document.querySelector('#hoverEl')
+const visitedEl = document.querySelector('#visitedEl')
+const dropStatus = document.querySelector('#dropStatus')
+
+defaultEl.addEventListener('click', () => {
+  dropStatus.innerHTML = 'Default'
+  status = 'default'
+})
+
+activeEl.addEventListener('click', () => {
+  dropStatus.innerHTML = 'Active'
+  status = 'active'
+})
+
+focusEl.addEventListener('click', () => {
+  dropStatus.innerHTML = 'Focus'
+  status = 'focus'
+})
+
+hoverEl.addEventListener('click', () => {
+  dropStatus.innerHTML = 'Hover'
+  status = 'hover'
+})
+
+visitedEl.addEventListener('click', () => {
+  dropStatus.innerHTML = 'Visited'
+  status = 'visited'
+})
+
+
 const buttonEl = document.querySelector('#buttonEl')
 const aEl = document.querySelector('#aEl')
 const divEl = document.querySelector('#divEl')
@@ -43,54 +77,65 @@ const h3El = document.querySelector('#h3El')
 const pEl = document.querySelector('#pEl')
 const drop = document.querySelector('#drop')
 let boton = document.querySelector('#boton')
+let element = 'button';
 const menuHtml = new Collapse(drop)
 
 drop.addEventListener('click', () => { menuHtml.expand()})
 
 buttonEl.addEventListener('click', () => {
     drop.innerHTML = 'Button'
+    element = 'button'
     hidden('button', 'Button')
 })
 
 aEl.addEventListener('click', () => {
     drop.innerHTML = 'Anchor'
+    element = 'a'
     hidden('a', 'a Element')
 })
 
 divEl.addEventListener('click', () => {
     drop.innerHTML = 'Div'
+    element = 'div'
     hidden('div', 'Div Element')
 })
 
 imgEl.addEventListener('click', () => {
     drop.innerHTML = 'Img'
+    element = 'img'
     hidden('img', 'img')
 })
 
 h1El.addEventListener('click', () => {
     drop.innerHTML = 'H1'
+    element = 'h1'
     hidden('h1', 'H1 Element')
 })
 
 h2El.addEventListener('click', () => {
     drop.innerHTML = 'H2'
+    element = 'h2'  
     hidden('h2', 'H2 Element')
 })
 
 h3El.addEventListener('click', () => {
     drop.innerHTML = 'H3'
+    element = 'h3'
     hidden('h3', 'H3 Element')
 })
 
 pEl.addEventListener('click', () => {
     drop.innerHTML = 'Paragraph'
+    element = 'p'
     hidden('p', 'Paragraph')
 })
 
 const hidden = (e, txt) => {
   const newEl = document.createElement(e)
+  if(e === 'a') newEl.href = '#'
   newEl.textContent = txt
   newEl.id = "boton"
+  newEl.classList = boton.classList
   if(dark) {
     newEl.classList.remove('text-black')
     newEl.classList.add('text-white')
@@ -104,6 +149,7 @@ const hidden = (e, txt) => {
 
 /********************************************** */
 
+const root = document.querySelector('html')
 const body = document.querySelector('body')
 const theme = document.querySelector('#theme')
 const colores = document.querySelector('#colores')
@@ -236,61 +282,83 @@ padding.forEach(color => {
 })
 
 colors.forEach(color => {
-  colores.add(op(color, true))
+  colores.add(op(color, true, 'text'))
 })
 
 bg.forEach(color => {
   bgs.add(op(color))
 })
 
-const change = (color, e) => {
+const change = (color, e, select) => {
+  let statusClass;
+  if(status === 'default') statusClass = color
+  if(status === 'active') {statusClass = 'active:' + color; select = 'active'}
+  if(status === 'focus') {statusClass = 'focus:' + color; select = 'focus'}
+  if(status === 'hover') {statusClass = 'hover:' + color; select = 'hover'}
+  if(status === 'visited') {statusClass = 'visited:' + color; select = 'visited'}
   clases.forEach(clase => {
     if(clase['el'] === e) {
-      if (clase['clase']) boton.classList.remove(clase['clase'])
-      clase['clase'] = color
+      if(clase['clase']){
+        if (clase['clase'].startsWith(select)) boton.classList.remove(clase['clase'])
+        if(e === 'decoration' && clase['clase']) boton.classList.remove(clase['clase'])
+      }
+      clase['clase'] = statusClass
       boton.classList.add(clase['clase'])
     }
   })
+  const co = document.querySelector('#code')
+  co.textContent = boton.outerHTML
+  boton.addEventListener('click', () => {
+    const temp = document.createElement('input')
+    body.append(temp)
+    temp.value = co.textContent
+    temp.select()
+    document.execCommand('copy')
+    document.body.removeChild(temp)
+  })
+  boton.removeEventListener('click', () =>{})
 }
 
 colores.addEventListener('change', () => {
   dark ? boton.classList.remove('text-white') : boton.classList.remove('text-black')
-  change(colores.value, 'colors'); 
+  change(colores.value, 'colors', 'text'); 
 })
-bgs.addEventListener('change', () => {change(bgs.value, 'bg')})
-padd.addEventListener('change', () => {change(padd.value, 'padding')})
-brd_w.addEventListener('change', () => {change(brd_w.value, 'border_width')})
-brd_c.addEventListener('change', () => {change(brd_c.value, 'border_color')})
-brd_r.addEventListener('change', () => {change(brd_r.value, 'border_radius')})
-brd_s.addEventListener('change', () => {change(brd_s.value, 'border_style')})
-brd_o.addEventListener('change', () => {change(brd_o.value, 'border_opacity')})
-font.addEventListener('change', () => {change(font.value, 'fontsize')})
+bgs.addEventListener('change', () => {change(bgs.value, 'bg', 'bg')})
+padd.addEventListener('change', () => {change(padd.value, 'padding', 'p')})
+brd_w.addEventListener('change', () => {change(brd_w.value, 'border_width', 'border')})
+brd_c.addEventListener('change', () => {change(brd_c.value, 'border_color', 'border')})
+brd_r.addEventListener('change', () => {change(brd_r.value, 'border_radius', 'rounded')})
+brd_s.addEventListener('change', () => {change(brd_s.value, 'border_style', 'border')})
+brd_o.addEventListener('change', () => {change(brd_o.value, 'border_opacity', 'border')})
+font.addEventListener('change', () => {change(font.value, 'fontsize', 'text')})
 t_decoration.addEventListener('change', () => {change(t_decoration.value, 'decoration')})
-shadow.addEventListener('change', () => {change(shadow.value, 'shadows')})
-text_align.addEventListener('change', () => {change(text_align.value, 'text_aligns')})
-width.addEventListener('change', () => {change(width.value, 'widths')})
-height.addEventListener('change', () => {change(height.value, 'heights')})
-scale.addEventListener('change', () => {change(scale.value, 'scales')})
-rotate.addEventListener('change', () => {change(rotate.value, 'rotates')})
-translate.addEventListener('change', () => {change(translate.value, 'translates')})
-brightnes.addEventListener('change', () => {change(brightnes.value, 'brightness')})
-contrast.addEventListener('change', () => {change(contrast.value, 'contrasts')})
-blur.addEventListener('change', () => {change(blur.value, 'blurs')})
-hue.addEventListener('change', () => {change(hue.value, 'hues')})
-weight.addEventListener('change', () => {change(weight.value, 'weights')})
-opacity.addEventListener('change', () => {change(opacity.value, 'opacitys')})
-cursor.addEventListener('change', () => {change(cursor.value, 'cursors')})
+shadow.addEventListener('change', () => {change(shadow.value, 'shadows', 'shadow')})
+text_align.addEventListener('change', () => {change(text_align.value, 'text_aligns', 'text')})
+width.addEventListener('change', () => {change(width.value, 'widths', 'w')})
+height.addEventListener('change', () => {change(height.value, 'heights', 'h')})
+scale.addEventListener('change', () => {change(scale.value, 'scales', 'scale')})
+rotate.addEventListener('change', () => {change(rotate.value, 'rotates', 'rotate')})
+translate.addEventListener('change', () => {change(translate.value, 'translates', 'translates')})
+brightnes.addEventListener('change', () => {change(brightnes.value, 'brightness', 'brightness')})
+contrast.addEventListener('change', () => {change(contrast.value, 'contrasts', 'contrast')})
+blur.addEventListener('change', () => {change(blur.value, 'blurs', 'blur')})
+hue.addEventListener('change', () => {change(hue.value, 'hues', 'hue')})
+weight.addEventListener('change', () => {change(weight.value, 'weights', 'font')})
+opacity.addEventListener('change', () => {change(opacity.value, 'opacitys', 'opacity')})
+cursor.addEventListener('change', () => {change(cursor.value, 'cursors', 'cursor')})
 
 theme.addEventListener('click', () => {
   if(dark){
     body.classList.remove('bg-gray-900')
     boton.classList.remove('text-white')
+    body.classList.remove('dark')
     body.classList.add('bg-[#eee]')
     boton.classList.add('text-black')
   } else {
     body.classList.remove('bg-[#eee]')
     boton.classList.remove('text-black')
     body.classList.add('bg-gray-900')
+    body.classList.add('dark')
     boton.classList.add('text-white')
   }
   dark = !dark
